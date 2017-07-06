cd8e47095daca8a741967eca45afcb0c117ea485
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2017-07-06 16:25:02 +0200

    add a method for listing tresor's members

--- signer_test.go
+++ signer_test.go
@@ -40,7 +40,6 @@ import (
 )
 
 const (
-	AdminUserId    = "admin@exampletenant.tresorit.io"
 	AdminKey       = "204bcf1b"
 	Sha256HexEmpty = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 )
