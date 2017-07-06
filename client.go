a06111ffeb6874acf34293081077ea8e45525aec
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2017-07-06 16:22:25 +0200

    extract the signing logic to separate struct

--- client.go
+++ client.go
@@ -31,136 +31,39 @@
 package zerokit
 
 import (
-	"bytes"
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/hex"
+	"github.com/go-errors/errors"
 	"net/http"
-	"strings"
-	"time"
 )
 
-type ZeroKitAdminAPIClient struct {
-	http.Client
-	ServiceUrl  string
-	AdminUserId string
-	AdminKey    string
+type tresoritClient struct {
+	requestSigner
+	httpClient httpClient
+	serviceUrl string
 }
 
-func (z *ZeroKitAdminAPIClient) SignAndDo(r *http.Request,
-	b []byte) (*http.Response, error) {
-	err := z.sign(r, b)
+type httpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+func NewTresoritClient(serviceUrl, adminUserId, adminKey string) (*tresoritClient, error) {
+	if serviceUrl == "" || adminKey == "" || adminUserId == "" {
+		return nil, errors.New("one or more arguments are empty")
+	}
+
+	return &tresoritClient{
+		requestSigner: requestSigner{
+			adminKey:    adminKey,
+			adminUserId: adminUserId,
+		},
+		httpClient: http.DefaultClient,
+		serviceUrl: serviceUrl,
+	}, nil
+}
+
+func (c *tresoritClient) SignAndDo(req *http.Request) (*http.Response, error) {
+	err := c.sign(req)
 	if err != nil {
 		return nil, err
 	}
-	return z.Do(r)
-}
-
-// As all server-side APIs of the Tresorit platform are stateless HTTP services,
-// all requests made against it have to be authenticated individually. This is
-// done by signing all request with the Tenant-specific admin key with the
-// special scheme described below:
-//
-// 1. Assemble the request headers:
-//
-//     - Content-Type: application/json (Post only)
-//     - Content-SHA256: <sha256hex of the request body> (Post only)
-//     - TresoritDate: <timestamp in ISO-8601>
-//     - UserId: <tenant admin user id>
-//     - HMACHeaders: <comma separated headers>
-//
-// 2. Assemble the canonicalized request string, which will be the subject of
-// the signing. The canonical string complies to the following format:
-//
-// 		<request verb>\n +
-// 		<path>[?key=value [& ...] ]\n +
-// 		header:value[\n ...]
-//
-// The headers key-value pair must be listed in the same order as in the
-// HMACHeaders header.
-//
-// 3. Sing the request. The algorithm is the following:
-//
-//     BASE64ENCODE(
-//     		HMACSHA256(
-//     			key=HEXTOBIN(AdminKey),
-//     			data=UTF8TOBINARY(Canonical Request String)
-//     		)
-//     )
-//
-// 4. Add authorization header:
-//
-// 		Authorization: AdminKey <SignatureBase64>
-//
-// where SignatureBase64 is the previously computed signature of the canonical
-// request string.
-func (z *ZeroKitAdminAPIClient) sign(req *http.Request, content []byte) error {
-	if req.Method == "POST" {
-		req.Header["Content-Type"] = []string{"application/json"}
-		req.Header["Content-SHA256"] = []string{sha256hex(content)}
-	}
-
-	// The Add and Set methods of http.Header canonicalize header names when
-	// adding values to the header map, which result in the incorrect signature
-	// validation by the tresorit API. Therefore, we bypass the behavior of the
-	// Set and Get by setting the headers using map operation.
-	//req.Header["Content-Length"] = []string{strconv.Itoa(len(content))}
-	req.Header["TresoritDate"] = []string{time.Now().UTC().Format(time.RFC3339)}
-	req.Header["UserId"] = []string{z.AdminUserId}
-	req.Header["HMACHeaders"] = []string{}
-
-	var headers []string
-	for k := range req.Header {
-		headers = append(headers, k)
-	}
-	req.Header["HMACHeaders"] = []string{strings.Join(headers, ",")}
-
-	// assemble a canonicalized string of the requests
-	var buffer bytes.Buffer
-	buffer.WriteString(req.Method + "\n")
-	buffer.WriteString(strings.TrimPrefix(req.URL.Path, "/"))
-	if req.URL.RawQuery != "" {
-		buffer.WriteString("?" + req.URL.RawQuery)
-	}
-	for _, key := range headers {
-		buffer.WriteString("\n")
-		buffer.WriteString(key)
-		buffer.WriteString(":")
-		buffer.WriteString(req.Header[key][0])
-
-	}
-
-	// sign the canonicalized string of the requests
-	sig, err := computeHmacSHA256([]byte(buffer.String()), z.AdminKey)
-	if err != nil {
-		return err
-	}
-
-	// Set authorization header.
-	req.Header["Authorization"] = []string{
-		"AdminKey " + base64.StdEncoding.EncodeToString(sig),
-	}
-	return nil
-}
-
-// The function to compute the keyed-hash message authentication code (HMAC)
-// of the input data, keyed with the binary input key and using the SHA256
-// algorithm as hash function.
-func computeHmacSHA256(data []byte, secret string) ([]byte, error) {
-	key := []byte(secret)
-	dst := make([]byte, hex.DecodedLen(len(key)))
-	_, err := hex.Decode(dst, key)
-	if err != nil {
-		return nil, err
-	}
-	h := hmac.New(sha256.New, dst)
-	h.Write(data)
-	return h.Sum(nil), nil
-}
-
-func sha256hex(content []byte) string {
-	h := sha256.New()
-	h.Write(content)
-	return hex.EncodeToString(h.Sum(nil))
+	return c.httpClient.Do(req)
 }
